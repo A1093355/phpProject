6d17b3ffe80eca96bbb42c2b10bd2ae6348e2a8f
Yu-Ting <100353432+A1093355@users.noreply.github.com>
2022-05-04 20:12:20 +0800

    5/4
    photoupdate

--- updateConfirm.php
+++ updateConfirm.php
@@ -1,19 +1,21 @@
 <?php
 require("DBconnect.php");
-$id=$_POST["id"];
-$name=$_POST["name"];
-$pwd=$_POST["pwd"];
-$role=$_POST["role"];
-$SQL="UPDATE user SET name='$name', pwd='$pwd', role='$role' WHERE id='$id'";
+$uNo=$_POST["id"];
+$uName=$_POST["name"];
+$uPwd=$_POST["pwd"];
+$uRole=$_POST["role"];
+
+$SQL="UPDATE user SET name='$uName' , pwd='$uPwd' , role='$uRole' WHERE id='$uNo'";
+
 if(mysqli_query($link, $SQL)){
     echo "<script type='text/javascript'>";
-    echo "alert('更新成功')";
+    echo "alert('更新成功');";
     echo "</script>";
-    echo "<meta http-equiv='Refresh' content='0'; url='list.php'>";
+    echo "<meta http-equiv='Refresh' content='0; url=list.php'>";
 }else{
     echo "<script type='text/javascript'>";
-    echo "alert('更新失敗')";
+    echo "alert('更新失敗');";
     echo "</script>";
-    echo "<meta http-equiv='Refresh' content='0'; url='updateS.php'>";
+    echo "<meta http-equiv='Refresh' content='0; url=list.php'>";
 }
 ?>
